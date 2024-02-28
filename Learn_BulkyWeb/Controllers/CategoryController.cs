95131022fed9c2ebb891b9d60469386b24d5eb5c
ChangKong <songmaverick@163.com>
2024-02-28 21:55:00 +0800

    更新到02-14

---
+++ Learn_BulkyWeb/Controllers/CategoryController.cs
@@ -0,0 +1,21 @@
+﻿using Learn_BulkyWeb.Data;
+using Learn_BulkyWeb.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Learn_BulkyWeb.Controllers {
+    public class CategoryController : Controller {
+
+        private readonly ApplicationDbContext _db;
+
+        public CategoryController(ApplicationDbContext dbContext) {
+            
+            _db = dbContext;
+        }
+
+        public IActionResult Index() {
+
+            List<Category> categoryList = _db.Category.ToList();
+            return View(categoryList);
+        }
+    }
+}
